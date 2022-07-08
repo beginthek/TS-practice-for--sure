cd57567ad5598a030cded22d00ce2ef19f1ee167
sungyun kim <yunkim1992@gmail.com>
2022-07-08 23:25:52 +0900

    집에서

--- 3-OOP/3-2-class.ts
+++ 3-OOP/3-2-class.ts
@@ -7,11 +7,13 @@
     
     class CoffeeMaker{
         BEANS_GRAMM_PER_SHOT:number = 7; 
-        coffeeBeans: number = 0;
+        coffeeBeans:number = 0;
 
-        constructor(coffeeBeans: ){
-            
-        }
+        constructor(coffeeBeans: number){
+            this.coffeeBeans = coffeeBeans
+        } //constructor 라는 함수는 클래스를 가지고 object(인스턴스)를 만들때
+        //항상 호출되는 함수다.
+
 
         makeCoffee(shots: number):CoffeeCup{
             if(this.coffeeBeans < shots * this.BEANS_GRAMM_PER_SHOT){
@@ -22,10 +24,11 @@
                 shots, // key , valuee 같을떄는 생략가능 
                 hasMilk: false,
             }
-          
+
         }
     }
-    const maker= new CoffeeMaker();
+    const maker = new CoffeeMaker(32); //new 라는 것은 이클래스의 인스턴스를 만든다는 뜻
+                                        //클래스 뒤에 괄호는 생성자를 호출한다는 뜻 
 }
 
 
